3b06a7f37d940f1a5b7f523ae33749e081998a3d
Berserk-M <nastia__@mail.ru>
2021-01-31 14:59:01 +0300

    Shuffle algrorythm

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# taro-oracle
+# Tarot reading
+
+I used Fisher–Yates Shuffle algorithm in this work
+link:
+https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+https://bost.ocks.org/mike/shuffle/
--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-"use strict";console.log("Hello world!"),console.log("Hello world1!");
+"use strict";
+
+let arrOfCards = [1,2,3,4,5,6,7,8,9];
+
+
+const shuffle = (array,num) => {
+	let arrShuffle = array.slice();
+	let currentIndex = arrShuffle.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = arrShuffle[currentIndex];
+		arrShuffle[currentIndex] = arrShuffle[randomIndex];
+		arrShuffle[randomIndex] = temporaryValue;
+	}
+	return arrShuffle.slice(0,num);
+}
+
+console.log(shuffle(arrOfCards,3))
