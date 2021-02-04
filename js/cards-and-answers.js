cf0058cc74d85d886ab43427c7486198f58f209f
Berserk-M <nastia__@mail.ru>
2021-02-05 00:20:26 +0300

    Markup details & script
    @media css
    js DOM events

--- js/cards-and-answers.js
+++ js/cards-and-answers.js
@@ -6,92 +6,88 @@
 
 const tarot = {
 	'card_1':{
-		url: '../assets/cards/card_1',
+		url: 'assets/cards/card_1.png',
 		description: 'description',
 		meaning: 'funny meaning',
 	},
 	'card_2':{
-		url: '../assets/cards/card_2',
+		url: 'assets/cards/card_2.png',
 		description: 'description',
 		meaning: 'funny meaning',
 	},
 	'card_3':{
-		url: '../assets/cards/card_3',
+		url: 'assets/cards/card_3.png',
 		description: 'description',
 		meaning: 'funny meaning',
 	},
 	'card_4':{
-		url: '../assets/cards/card_4',
+		url: 'assets/cards/card_4.png',
 		description: 'description',
 		meaning: 'funny meaning',
 	},
 	'card_5':{
-		url: '../assets/cards/card_5',
+		url: 'assets/cards/card_5.png',
 		description: 'description',
 		meaning: 'funny meaning',
 	},
 	'card_6':{
-		url: '../assets/cards/card_6',
+		url: 'assets/cards/card_6.png',
 		description: 'description',
 		meaning: 'funny meaning',
 	},
 	'card_7':{
-		url: '../assets/cards/card_7',
+		url: 'assets/cards/card_7.png',
 		description: 'description',
 		meaning: 'funny meaning',
 	},
 	'card_8':{
-		url: '../assets/cards/card_8',
+		url: 'assets/cards/card_8.png',
 		description: 'description',
 		meaning: 'funny meaning',
 	},
 	'card_9':{
-		url: '../assets/cards/card_9',
+		url: 'assets/cards/card_9.png',
 		description: 'description',
 		meaning: 'funny meaning',
 	},
 	'card_10':{
-		url: '../assets/cards/card_10',
+		url: 'assets/cards/card_10.png',
 		description: 'description',
 		meaning: 'funny meaning',
 	},
 	'card_11':{
-		url: '../assets/cards/card_11',
+		url: 'assets/cards/card_11.png',
 		description: 'description',
 		meaning: 'funny meaning',
 	},
 	'card_12':{
-		url: '../assets/cards/card_12',
+		url: 'assets/cards/card_12.png',
 		description: 'description',
 		meaning: 'funny meaning',
 	},
 	'card_13':{
-		url: '../assets/cards/card_13',
+		url: 'assets/cards/card_13.png',
 		description: 'description',
 		meaning: 'funny meaning',
 	},
 	'card_14':{
-		url: '../assets/cards/card_14',
+		url: 'assets/cards/card_14.png',
 		description: 'description',
 		meaning: 'funny meaning',
 	},
 	'card_15':{
-		url: '../assets/cards/card_15',
+		url: 'assets/cards/card_15.png',
 		description: 'description',
 		meaning: 'funny meaning',
 	},
 	'card_16':{
-		url: '../assets/cards/card_16',
+		url: 'assets/cards/card_16.png',
 		description: 'description',
 		meaning: 'funny meaning',
 	},
 	'card_17':{
-		url: '../assets/cards/card_17',
+		url: 'assets/cards/card_17.png',
 		description: 'description',
 		meaning: 'funny meaning',
 	},
 }
-
-//console.log(tarot.first.meaning);
-
-
--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 "use strict";
-
+//
+//
+//
+// Logic
 
 // Function based on Fisher–Yates Shuffle algorithm
 const shuffle = (array,num=3) => {
@@ -18,15 +21,48 @@ const shuffle = (array,num=3) => {
 	return arrShuffle.slice(0,num);
 }
 
-
 // Keys of object of Taro Cards
 let keysTarotArr = Object.keys(tarot);
 
-//Shuffle cards's keys
-let resultArr = shuffle(keysTarotArr);
+// Variables
+let resultArr, firstCardURL, secondCardURL, thirdCardURL;
 
-let firstCardURL = tarot[resultArr[0]].url;
-let secondCardURL = tarot[resultArr[0]].url;
-let thirdCardURL = tarot[resultArr[0]].url;
 
-console.log(resultArr);
+//
+//
+//
+//
+// Actions with DOM
+
+let buttonGame = document.querySelector('.bottom-block__button');
+let result = document.querySelector('.bottom-block__output');
+let tryAgain = document.querySelector('.output-block__button');
+
+let img1 = document.querySelector('#card-item1');
+let img2 = document.querySelector('#card-item2');
+let img3 = document.querySelector('#card-item3');
+
+
+buttonGame.addEventListener('click', function(){
+	this.classList.add('disabled');
+	result.classList.remove('disabled');
+
+	resultArr = shuffle(keysTarotArr);
+	firstCardURL = tarot[resultArr[0]].url;
+	secondCardURL = tarot[resultArr[1]].url;
+	thirdCardURL = tarot[resultArr[2]].url;
+
+	img1.src = firstCardURL;
+	img2.src = secondCardURL;
+	img3.src = thirdCardURL;
+});
+
+
+tryAgain.addEventListener('click', function(){
+	result.classList.add('disabled');
+	buttonGame.classList.remove('disabled');
+
+	img1.src = 'assets/cards/card_1.png';
+	img2.src = 'assets/cards/card_2.png';
+	img3.src = 'assets/cards/card_3.png';	
+});
